2816b5935fafff70e74d83e537598ffef3d4712e
PingedGuin <pingedguinql@gmail.com>
2026-02-24 16:01:52 +0300

    Add basic authentication interceptor for API endpoint protection

---
+++ src/main/java/dev/dre/chatappserver/apis/Interceptor/AuthInterceptor.java
@@ -0,0 +1,18 @@
+package dev.dre.chatappserver.apis.Interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class AuthInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+
+        if (token == null || !token.equals("token")) {
+            response.sendError(403);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/main/java/dev/dre/chatappserver/apis/config/WebConfig.java
@@ -0,0 +1,14 @@
+package dev.dre.chatappserver.apis.config;
+
+import dev.dre.chatappserver.apis.Interceptor.AuthInterceptor;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new AuthInterceptor())
+                .addPathPatterns("/api/**");
+    }
+}
