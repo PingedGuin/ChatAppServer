9434b9ac86aa076b1a41fe6325308e5d6b491c30
PingedGuin <pingedguinql@gmail.com>
2026-02-25 15:40:24 +0300

    Update `LoginController` to use `@PostMapping` for `/api/auth/login` and remove `@Valid` and unnecessary `@RequestHeader`. Add debug logging with `System.out.println`.

--- src/main/java/dev/dre/chatappserver/apis/register/LoginController.java
+++ src/main/java/dev/dre/chatappserver/apis/register/LoginController.java
@@ -2,15 +2,16 @@ package dev.dre.chatappserver.apis.register;
 
 import dev.dre.chatappserver.ChatAppServerApplication;
 import dev.dre.chatappserver.dtos.LoginRequest;
-import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class LoginController {
-    @GetMapping("/api/auth/login")
-    public ResponseEntity<?> login(@Valid @RequestHeader("Authorization") String token, @RequestBody LoginRequest request) {
+    @PostMapping("/api/auth/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        System.out.println(request);
         if (ChatAppServerApplication.getDatabase().isUserExist(request.getUsername())){
             return ResponseEntity.ok().body("logged in");
         }
